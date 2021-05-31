fa2b235f28756bbac4235f78c7b902ff6545d358
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-31 09:52:38 -0400

    some id added

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/home');
+    } else {
+      alert('Failed to log in.');
+    }
+  }
+};
+
+document
+  .querySelector('.sign-in-form')
+  .addEventListener('submit', loginFormHandler);
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/home');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+
+document
+  .querySelector('.sign-up-form')
+  .addEventListener('submit', signupFormHandler);
