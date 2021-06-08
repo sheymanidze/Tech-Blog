6258b8429dc92d029b45f2e3966a06f17627231c
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-06-07 21:20:12 -0400

    seeding, models added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+//const { Commenst, Post, User } = require('../models');
 
 router.get("/", (req, res) => {
   if (req.session.loggedIn) {
---
+++ models/index.js
@@ -0,0 +1,29 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comments = require('./Comments')
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comments, {
+  foreignKey: "post_id"
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Commenst.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comments, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Commenst };
